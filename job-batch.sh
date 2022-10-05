736528f0b425f271b0ff8ae3e500cae6245e7202
Jiang Zifan (zifjia) <zifjia@login2>
2022-10-05 20:50:22 +0200

    results of transformer

--- job-batch.sh
+++ job-batch.sh
@@ -2,10 +2,14 @@
 # sbatch --output=models/chimp_waveform_lstm_42/train.log ./job.sh models/chimp_waveform_lstm_42/config.json
 # sbatch --output=models/chimp_waveform_lstm_3407/train.log ./job.sh models/chimp_waveform_lstm_3407/config.json
 
-sbatch --output=models/chimp_spectrogram_lstm_0/train.log ./job.sh models/chimp_spectrogram_lstm_0/config.json
-sbatch --output=models/chimp_spectrogram_lstm_42/train.log ./job.sh models/chimp_spectrogram_lstm_42/config.json
-sbatch --output=models/chimp_spectrogram_lstm_3407/train.log ./job.sh models/chimp_spectrogram_lstm_3407/config.json
+# sbatch --output=models/chimp_spectrogram_lstm_0/train.log ./job.sh models/chimp_spectrogram_lstm_0/config.json
+# sbatch --output=models/chimp_spectrogram_lstm_42/train.log ./job.sh models/chimp_spectrogram_lstm_42/config.json
+# sbatch --output=models/chimp_spectrogram_lstm_3407/train.log ./job.sh models/chimp_spectrogram_lstm_3407/config.json
 
-sbatch --output=models/chimp_wav2vec2_lstm_0/train.log ./job.sh models/chimp_wav2vec2_lstm_0/config.json
-sbatch --output=models/chimp_wav2vec2_lstm_42/train.log ./job.sh models/chimp_wav2vec2_lstm_42/config.json
-sbatch --output=models/chimp_wav2vec2_lstm_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_3407/config.json
+# sbatch --output=models/chimp_wav2vec2_lstm_0/train.log ./job.sh models/chimp_wav2vec2_lstm_0/config.json
+# sbatch --output=models/chimp_wav2vec2_lstm_42/train.log ./job.sh models/chimp_wav2vec2_lstm_42/config.json
+# sbatch --output=models/chimp_wav2vec2_lstm_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_3407/config.json
+
+sbatch --output=models/chimp_wav2vec2_transformer_0/train.log ./job.sh models/chimp_wav2vec2_transformer_0/config.json
+sbatch --output=models/chimp_wav2vec2_transformer_42/train.log ./job.sh models/chimp_wav2vec2_transformer_42/config.json
+sbatch --output=models/chimp_wav2vec2_transformer_3407/train.log ./job.sh models/chimp_wav2vec2_transformer_3407/config.json
