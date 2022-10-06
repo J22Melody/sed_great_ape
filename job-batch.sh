6d7c270b8ddc4f98ca57c490c35367506ffb5350
Jiang Zifan (zifjia) <zifjia@login0>
2022-10-06 20:45:53 +0200

    results from balance weight exp.

--- job-batch.sh
+++ job-batch.sh
@@ -22,10 +22,14 @@
 # sbatch --output=models/chimp_wav2vec2_lstm_batch_8_42/train.log ./job.sh models/chimp_wav2vec2_lstm_batch_8_42/config.json
 # sbatch --output=models/chimp_wav2vec2_lstm_batch_8_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_batch_8_3407/config.json
 
-sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.2_0/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.2_0/config.json
-sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.2_42/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.2_42/config.json
-sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.2_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.2_3407/config.json
+# sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.2_0/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.2_0/config.json
+# sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.2_42/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.2_42/config.json
+# sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.2_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.2_3407/config.json
 
-sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.1_0/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.1_0/config.json
-sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.1_42/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.1_42/config.json
-sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.1_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.1_3407/config.json
+# sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.1_0/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.1_0/config.json
+# sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.1_42/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.1_42/config.json
+# sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.1_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.1_3407/config.json
+
+sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.2_bw_0/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.2_bw_0/config.json
+sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.2_bw_42/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.2_bw_42/config.json
+sbatch --output=models/chimp_wav2vec2_lstm_dropout_0.2_bw_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_dropout_0.2_bw_3407/config.json
