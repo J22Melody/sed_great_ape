97cabf8d68724fafaaaf1dcc76d33543cb73d7ad
Jiang Zifan (zifjia) <zifjia@login0>
2022-10-06 19:56:55 +0200

    result batch size

--- job-batch.sh
+++ job-batch.sh
@@ -10,6 +10,14 @@
 # sbatch --output=models/chimp_wav2vec2_lstm_42/train.log ./job.sh models/chimp_wav2vec2_lstm_42/config.json
 # sbatch --output=models/chimp_wav2vec2_lstm_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_3407/config.json
 
-sbatch --output=models/chimp_wav2vec2_transformer_0/train.log ./job.sh models/chimp_wav2vec2_transformer_0/config.json
-sbatch --output=models/chimp_wav2vec2_transformer_42/train.log ./job.sh models/chimp_wav2vec2_transformer_42/config.json
-sbatch --output=models/chimp_wav2vec2_transformer_3407/train.log ./job.sh models/chimp_wav2vec2_transformer_3407/config.json
+# sbatch --output=models/chimp_wav2vec2_transformer_0/train.log ./job.sh models/chimp_wav2vec2_transformer_0/config.json
+# sbatch --output=models/chimp_wav2vec2_transformer_42/train.log ./job.sh models/chimp_wav2vec2_transformer_42/config.json
+# sbatch --output=models/chimp_wav2vec2_transformer_3407/train.log ./job.sh models/chimp_wav2vec2_transformer_3407/config.json
+
+sbatch --output=models/chimp_wav2vec2_lstm_batch_4_0/train.log ./job.sh models/chimp_wav2vec2_lstm_batch_4_0/config.json
+sbatch --output=models/chimp_wav2vec2_lstm_batch_4_42/train.log ./job.sh models/chimp_wav2vec2_lstm_batch_4_42/config.json
+sbatch --output=models/chimp_wav2vec2_lstm_batch_4_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_batch_4_3407/config.json
+
+sbatch --output=models/chimp_wav2vec2_lstm_batch_8_0/train.log ./job.sh models/chimp_wav2vec2_lstm_batch_8_0/config.json
+sbatch --output=models/chimp_wav2vec2_lstm_batch_8_42/train.log ./job.sh models/chimp_wav2vec2_lstm_batch_8_42/config.json
+sbatch --output=models/chimp_wav2vec2_lstm_batch_8_3407/train.log ./job.sh models/chimp_wav2vec2_lstm_batch_8_3407/config.json
