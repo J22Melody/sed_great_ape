c9840291c575a24d61a4eab82fe43a7640953a44
Zifan Jiang <j22melody@gmail.com>
2022-10-19 13:53:55 +0200

    new analysis on greatarc longcall

--- data_greatarc_longcall/README.md
+++ data_greatarc_longcall/README.md
@@ -32,6 +32,31 @@ Total duration of the pulse level annotated files: 1036.7140136054422
  'Unknown': {'count': 58, 'duration': 8.97822408899999, 'id': 1}}
 ```
 
+## Analysis (New)
+
+The old analysis above based on the raven tables do not include complete annotations, the new analysis is based on the .xlsx annotation file.
+
+`python analysis_new.py`
+
+There are 76 audio files, 65 of which have pulse level annotation files (can be used for multi-class classification).
+
+Now have a look at the total duration (in second) and the total duration for each pulse level.
+
+For the 65 annotated files:
+
+```
+{'Bubble sub-pulse': {'count': 4422, 'duration': 329.68749999999926, 'id': 5},
+ 'Full pulse': {'count': 1929, 'duration': 1762.548500000001, 'id': 2},
+ 'Grumble sub-pulse': {'count': 757, 'duration': 45.81939999999997, 'id': 6},
+ 'Grumph': {'count': 9, 'duration': 2.5993999999999957, 'id': 8},
+ 'Kiss-squeak': {'count': 9, 'duration': 2.6621999999999844, 'id': 7},
+ 'Pulse body': {'count': 816, 'duration': 553.7037000000003, 'id': 4},
+ 'Sub-pulse transitory element': {'count': 1068,
+                                  'duration': 95.86570000000007,
+                                  'id': 3},
+ 'Unknown': {'count': 0, 'duration': 0, 'id': 1}}
+```
+
 ## Process
 
 `python process.py`
---
+++ data_greatarc_longcall/analysis_new.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+import pandas as pd
+import numpy as np
+import librosa
+import os
+import csv
+from pathlib import Path
+from pprint import pprint
+
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+WAV_PATH = './raw/LC rhythm - all LC Tuanan'
+ANNOTATION_PATH = './raw/ALL ELEMENTS Index.xlsx'
+
+df_raw = pd.read_excel(ANNOTATION_PATH, keep_default_na=False)
+
+data = []
+pulse_level_map = {'Unknown': {'id': 1, 'count': 0, 'duration': 0}}
+annotation_duration = 0
+
+wavpaths = Path(WAV_PATH).rglob('*.wav')
+
+for i, wavpath in enumerate(wavpaths):
+    wavpath = str(wavpath)
+    filename = wavpath.split('/')[-1].split('.')[0]
+    duration = librosa.get_duration(filename=wavpath)
+
+    # check whether annotations exist
+    df_current = df_raw[(df_raw['Master File Name'] == filename) & (df_raw['Pulse level'] != '')]
+    annotation_pulse_level = len(df_current) > 0
+
+    if annotation_pulse_level:
+        for item in df_current.to_dict('records'):
+            pulse_duration = float(item['End Time']) - float(item['Begin Time'])
+            annotation_duration = annotation_duration + pulse_duration
+
+            pulse_level = item['Pulse level'].strip() if item.get('Pulse level', '') else 'Unknown'
+
+            # fix typo
+            if pulse_level in ['Sub-pulse trnasitory element', 'Sub-pule transitory element', 'Sub-pulse transiotry element']:
+                pulse_level = 'Sub-pulse transitory element'
+            elif pulse_level in ['Bubble sub-pulseBubble sub-pulse', 'Buble sub-pulse']:
+                pulse_level = 'Bubble sub-pulse'
+            elif pulse_level in ['Pulse bpody', 'Puse body']:
+                pulse_level = 'Pulse body'
+
+            if pulse_level in pulse_level_map:
+                pulse_level_map[pulse_level]['count'] += 1
+                pulse_level_map[pulse_level]['duration'] += pulse_duration
+            else:
+                pulse_level_map[pulse_level] = {
+                    'count': 1,
+                    'duration': pulse_duration,
+                    'id': len(pulse_level_map.keys()) + 1,
+                }
+
+    data.append([filename, duration, annotation_pulse_level])
+
+df = pd.DataFrame(data, columns=['filename', 'duration', 'annotation_pulse_level'])
+df.to_csv('./analysis_new.csv')
+
+print('How many audio files? How many have pulse level anotations?')
+
+print(len(df))
+print(len(df[df['annotation_pulse_level'] == True]))
+
+print('How about duration?')
+
+print('annotation_duration:', annotation_duration)
+
+print('Total duration of the pulse level annotated files')
+
+print(df[df['annotation_pulse_level'] == True]['duration'].sum())
+
+pprint(pulse_level_map)
